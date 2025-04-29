9a6bdca02c2e82aed2a4dcea9a765a859a4cb565
Abd Eltawab Shaaban <105607290+scob337@users.noreply.github.com>
2025-04-29 20:47:28 +0300

    handle errors

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ const app = express();
 app.use(cors());
 app.use('/api/contact', contactRoutes);
 
+const allowedOrigins = [
+  'http://localhost:5000',  // لو أنت شغال على localhost
+  'https://divaniworld.com',
+  'http://localhost:3000'
+  
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    // إذا لم يكن الـ origin موجود، نسمح بالوصول
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST'], // تحديد الطرق المسموح بها
+}));
 const port = process.env.PORT || 5000;
 app.use(express.json());
 
